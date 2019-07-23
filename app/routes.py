2031e1b6b5dd724d2dca46dab959e111e939c9d5
Guowei Liu A <18042971250@189.cn>
2019-07-23 11:02:53 +0800

    使用模板，使用模板进行判断

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello word!"
+    user={'username':'lgw'}
+    return render_template('index.html', user=user)
