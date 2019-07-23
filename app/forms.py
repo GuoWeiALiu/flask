cda72d34f117948a496dcb5b845d41a4649157c8
Guowei Liu A <18042971250@189.cn>
2019-07-23 14:10:17 +0800

    接收表单数据

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired])
-    password = PasswordField('Password', validators=[DataRequired])
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from app import app
 from app.forms import LoginForm
@@ -7,20 +7,25 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user={'username':'lgw'}
+    user = {'username': 'lgw'}
     posts = [
         {
-            'author':{'username':'lgw'},
-            'body':'beautiful day in portland!'
+            'author': {'username': 'lgw'},
+            'body': 'beautiful day in portland!'
         },
         {
             'author': {'username': 'cjx'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', user=user, posts = posts)
+    return render_template('index.html', user=user, posts=posts)
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title = 'Sign In', form=form)
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
